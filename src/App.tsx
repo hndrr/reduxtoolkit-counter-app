d88e22408c69de2de5eb7bd9c72d5864d8e70d74
hndrr <yozhik20@gmail.com>
2023-02-04 05:41:40 +0900

    styled-components追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { RootState } from "./redux/store";
 import { decrement, increment, incrementByAmount } from "./redux/counterSlice";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
 
 function App() {
   const count = useSelector((state: RootState) => state.counter.value);
@@ -10,28 +11,83 @@ function App() {
   const [input, setInput] = useState(0);
 
   return (
-    <div className="App">
+    <Wrapper className="App">
       <h1>count: {count}</h1>
-      <input
+      <Input
         type="number"
         onChange={(e) => {
           setInput(Number(e.target.value));
         }}
-        value={input}
+        value={input === 0 ? "" : input}
       />
-      <button onClick={() => dispatch(increment())}>
-        <span>+</span>
-      </button>
-      <span> </span>
-      <button onClick={() => dispatch(decrement())}>
-        <span>-</span>
-      </button>
-      <span> </span>
-      <button onClick={() => dispatch(incrementByAmount(input))}>
+      <Spacer />
+      <Button
+        onClick={() => dispatch(incrementByAmount(input))}
+        disabled={input === 0}
+      >
         <span>add</span>
-      </button>
-    </div>
+      </Button>
+      <Spacer />
+      <IncrementButton onClick={() => dispatch(increment())}>
+        <span> + </span>
+      </IncrementButton>
+      <Spacer />
+      <DecrementButton onClick={() => dispatch(decrement())}>
+        <span> - </span>
+      </DecrementButton>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  text-align: center;
+`;
+const Input = styled.input`
+  width: 200px;
+  height: 50px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin: 8px 0;
+  outline: none;
+  padding: 12px 20px;
+  transition: all 0.5s;
+  &:focus {
+    border: 1px solid #555;
+  }
+`;
+
+const Button = styled.button`
+  background-color: #4caf50;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  &:disabled {
+    background-color: #ccc;
+    cursor: not-allowed;
+  }
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+const IncrementButton = styled(Button)`
+  background-color: red;
+`;
+
+const DecrementButton = styled(Button)`
+  background-color: blue;
+`;
+
+const Spacer = styled.span`
+  margin: 0 3px;
+`;
 
 export default App;
